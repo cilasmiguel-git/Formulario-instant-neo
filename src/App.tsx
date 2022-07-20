341a57ea877c5c8a2ff4525040019473831664bd
unknown <email>
2022-07-20 17:59:56 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const [name,setname] = useState('');
+  const [lastname,setlastname] = useState('')
+  const [fullname,setfullname]= useState('')
+  useEffect(
+    ()=>
+    {
+      setfullname(`${name} ${lastname}`);
+    },
+    [name,lastname]
+  )
+  const handlenamechange = ( e : ChangeEvent<HTMLInputElement> ) =>
+  {
+    setname(e.target.value);
+  }
+  const handlelastnamechange = ( e : ChangeEvent<HTMLInputElement> ) =>
+  {
+    setlastname(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header " >
+        <h1>Formulario instantâneo</h1>
+        <input type="text" placeholder='digite seu nome' value={name} onChange={handlenamechange}/>
+        <input type="text" placeholder='digite seu sobrenome' value={lastname} onChange={handlelastnamechange}/>
+        <p> Nome Completo : {fullname} </p>
       </header>
     </div>
   );
